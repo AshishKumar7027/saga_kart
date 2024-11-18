5a55d3c5b8e8162eca30ca8d57b3375bd6367d3d
Ashish <ashuusheoran@gmail.com>
2024-11-18 11:03:59 +0530

    storage helper added

---
+++ lib/core/storage_helper.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageHelper{
+  static const String tokenKey = 'tokenKey';
+  static Future saveToken(String token)async{
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setString(tokenKey, token);
+
+  }
+  static Future<String?> getToken()async{
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String? token = sp.getString(tokenKey);
+    return token;
+  }
+}
